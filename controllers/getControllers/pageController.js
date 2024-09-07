91d7aad8f4aef5614c2daecdbba0aa7856aa2669
Umar farooq <103427953+umarfarooq1901@users.noreply.github.com>
2024-09-07 17:42:11 +0530

    Added Logout button for user and Admin

--- controllers/getControllers/pageController.js
+++ controllers/getControllers/pageController.js
@@ -13,24 +13,35 @@ const getHomeHandler = (req, res) => {
 // Servies page route
 
 const getServicesHandler = (req, res) => {
-  res.render("Services", {
+  const token = req.cookies['token'];
+  const isLogged = !!token;
+  res.render('Services', {
+    isLogged,
     heading: "Our Services",
     subheading: "Comprehensive Travel Solutions Tailored to You",
   });
+
 };
 
 // contact route
 const getContactHandler = (req, res) => {
-  res.render("Contact", {
+  const token = req.cookies['token'];
+  const isLogged = !!token;
+  res.render('Contact',{
+    isLogged,
     heading: "Get in Touch with Us",
-    subheading:
-      "Feel free to reach out. We’re Here to Help Plan Your Next Adventure",
+    subheading:"Feel free to reach out. We’re Here to Help Plan Your Next Adventure",
+
   });
+
 };
 // packages route
 
 const getPackagesHandler = (req, res) => {
+  const token = req.cookies['token'];
+  const isLogged = !!token;
   res.render("Packages", {
+    isLogged,
     heading: "Travel Packages",
     subheading: "Explore Our Curated Experiences for Every Traveler",
   });
@@ -39,7 +50,10 @@ const getPackagesHandler = (req, res) => {
 // about route
 
 const getAboutHandler = (req, res) => {
+  const token = req.cookies['token'];
+  const isLogged = !!token;
   res.render("About", {
+    isLogged,
     heading: "Passion for Travel, Excellence in Service",
     subheading: "Passion for Travel, Excellence in Service",
   });
